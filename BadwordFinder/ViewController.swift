1e85cd76a5cb2088fb6a41f7c3f144574b28f549
YachWise <jacobhwiseios@gmail.com>
2024-02-07 15:27:07 -0600

    first commit

--- BadwordFinder/ViewController.swift
+++ BadwordFinder/ViewController.swift
@@ -8,9 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var words: [String]?
 
+    @IBOutlet weak var usernameTf: UITextField!
+    
+    @IBOutlet weak var detectedLabel: UILabel!
+    
+    @IBAction func confirmPressed(sender: UIButton)
+    {
+        if self.usernameTf.text?.isEmpty == true
+        {
+            
+        }
+        else
+        {
+            let combinedResult = words!.contains(where: self.usernameTf.text!.contains)
+
+            
+            if combinedResult == true
+            {
+                self.detectedLabel.text = "bad word"
+            }
+            else
+            {
+                self.detectedLabel.text = "no bad word"
+            }
+        }
+       
+    }
+    
+    
+    private func getWords()
+    {
+        if let wordsJson = Bundle.main.url(forResource: "words", withExtension: "json")
+        {
+            do
+            {
+                let data = try Data(contentsOf: wordsJson, options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                if let jsonResult = jsonResult as? [String]
+                {
+                    self.words = jsonResult
+                   
+                }
+            }
+            catch
+            {
+                print("error found")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getWords()
         // Do any additional setup after loading the view.
     }
 
